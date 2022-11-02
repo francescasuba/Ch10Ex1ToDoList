317a1e32b86fd3b16884c4f7a09784ae3a2f6cb9
subaap <subaap@VDI-CECH-IC-10>
2022-11-02 11:54:12 -0400

    Finished up to step 20

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -46,25 +46,27 @@ namespace ToDoList.Controllers
 
         public IActionResult Add()
         {
-            ViewBag.Categories = context.Categories.ToList();
-            ViewBag.Statuses = context.Statuses.ToList();
-            return View();
+            ToDoViewModel toDoViewModel = new ToDoViewModel();
+
+            toDoViewModel.Categories = context.Categories.ToList();
+            toDoViewModel.Statuses = context.Statuses.ToList();
+            return View(toDoViewModel);
         }
 
         [HttpPost]
-        public IActionResult Add(ToDo task)
+        public IActionResult Add(ToDoViewModel toDoViewModel)
         {
             if (ModelState.IsValid)
             {
-                context.ToDos.Add(task);
+                context.ToDos.Add(toDoViewModel.CurrentTask);
                 context.SaveChanges();
                 return RedirectToAction("Index");
             }
             else
             {
-                ViewBag.Categories = context.Categories.ToList();
-                ViewBag.Statuses = context.Statuses.ToList();
-                return View(task);
+                toDoViewModel.Categories = context.Categories.ToList();
+                toDoViewModel.Statuses = context.Statuses.ToList();
+                return View(toDoViewModel);
             }
         }
 
